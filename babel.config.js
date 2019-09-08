1bb8937fe99ed18fa8a1e91249c400c86b298a25
Jonathan Perry <jonathanperry@sequencemedia.net>
2019-09-08 11:53:22 +0100

    `rimraf`

--- babel.config.js
+++ babel.config.js
@@ -4,14 +4,14 @@ module.exports = {
   presets: [
     [
       '@babel/env', {
+        useBuiltIns: 'entry',
         targets: {
           node: 'current',
           browsers: [
             'last 2 versions'
           ]
         },
-        useBuiltIns: 'usage',
-        corejs: '3.0.1'
+        corejs: 3
       }
     ],
     '@babel/react'
