ff7a2f14c43e2aac8c5630c32669929968c4f416
Jonathan Perry <jonathanperry@sequencemedia.net>
2019-09-09 21:55:41 +0100

    Jest Enzyme

---
+++ src/motor/__tests__/index.test.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+import Motor from 'shinkansen-motor/motor'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('shinkansen-motor/motor', () => {
+  describe('Always', () => {
+    it('renders', () => {
+      expect(shallow(<Motor definition={{}} />))
+        .toMatchSnapshot()
+    })
+  })
+
+  describe('`definition` has `schema` object', () => {
+    it('renders', () => {
+      expect(shallow(<Motor definition={{ schema: {} }} />))
+        .toMatchSnapshot()
+    })
+  })
+
+  describe('`shouldComponentUpdate()`', () => {
+    const schema = {}
+
+    let instance
+    beforeEach(() => {
+      const wrapper = shallow(<Motor definition={{ schema }} />)
+
+      instance = wrapper.instance()
+    })
+
+    describe('`props` have changed', () => {
+      it('returns true', () => {
+        expect(instance.shouldComponentUpdate({ definition: { schema: {} } }))
+          .toBe(true)
+      })
+    })
+
+    describe('`props` have not changed', () => {
+      it('returns false', () => {
+        expect(instance.shouldComponentUpdate({ definition: { schema } }))
+          .toBe(false)
+      })
+    })
+  })
+})
